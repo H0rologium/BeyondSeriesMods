5df7d2944ddec19e47247d5af371ed98c8b31d22
H0rologium <murph1ethan@gmail.com>
2025-07-22 20:30:15 -0500

    fixed faction leader, added pawnkinddefs, tweaked raid values and tags. also added gitignore

---
+++ source/BetweenTheStars/FactionGenOptions.cs
@@ -0,0 +1,20 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace BetweenTheStars
+{
+    public class FactionGenOptions : DefModExtension
+    {
+        public List<PreceptDef> preceptsToAdd = new List<PreceptDef>();
+        public IdeoIconDef customIdeoIcon;
+        public ColorDef customIdeoColor;
+        public string customIdeoDescription;
+        public bool hasUniqueIdeo;
+    }
+
+}
