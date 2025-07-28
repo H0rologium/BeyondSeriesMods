338fb8494fe64498ea8a2f80f1b1d5d3b0e7ae56
H0rologium <murph1ethan@gmail.com>
2025-07-28 12:04:49 -0500

    added mod options, added new recipe, tweaked generation rates

---
+++ source/BetweenTheStars/BTSModSettings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace BetweenTheStars
+{
+    public class BTSModSettings : ModSettings
+    {
+        public bool passiveMode;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref passiveMode, "passiveMode", false);
+            base.ExposeData();
+        }
+    }
+
+    public class BetweenTheStars : Mod
+    {
+        BTSModSettings settings;
+
+        public BetweenTheStars(ModContentPack content) : base(content)
+        {
+            this.settings = GetSettings<BTSModSettings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard lst = new Listing_Standard();
+
+            lst.Begin(inRect);
+            lst.CheckboxLabeled("Enable passive mode", ref settings.passiveMode, "Reequires Reload\n\nPassive mode will allow you to bypass raiding faction bases and adds generation of the structure to tiles that you settle on or set up camp on.\n\nNote that this WILL make obtaining these resources much easier, however spawning rules will still be random and require Fabrication to start");
+
+            lst.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Between the Stars";
+        }
+    }
+}
--- source/BetweenTheStars/GenStep_Terminal.cs
+++ source/BetweenTheStars/GenStep_Terminal.cs
@@ -1,14 +1,22 @@
 ﻿using RimWorld;
+using RimWorld.Planet;
 using System;
 using System.Linq;
 using UnityEngine;
 using Verse;
 using Verse.AI;
+using Verse.Noise;
 
 namespace BetweenTheStars
 {
     public class GenStep_Terminal : Verse.GenStep
     {
+        private bool checkForPassive;
+
+        public GenStep_Terminal()
+        {
+            checkForPassive = LoadedModManager.GetMod<BetweenTheStars>().GetSettings<BTSModSettings>().passiveMode;
+        }
         public override int SeedPart
         {
             get
@@ -16,17 +24,42 @@ namespace BetweenTheStars
                 return 98641684;
             }
         }
+        private void GenTerminal(IntVec3 spIV,Map map)
+        {
+            atcDef = DefDatabase<ThingDef>.GetNamed("AncientTerminalQuantSegment", true);
+            GenSpawn.Spawn(this.atcDef, spIV, map, WipeMode.Vanish);
+            return;
+        }
 
         public override void Generate(Map map, GenStepParams parms)
         {
+
             if (ShouldSkipMap(map)) return;
             IntVec3 spIV;
             if (!TryFindCell(map, out spIV)) return;
             var spawnChance = UnityEngine.Random.Range(1.0f, 100.0f);
-            if (spawnChance < 48f || !InternalDefOf.Fabrication.IsFinished) { Log.Message("[BTS] Skipped terminal generation. this is normal behaviour."); return; }
-            atcDef = DefDatabase<ThingDef>.GetNamed("AncientTerminalQuantSegment", true);
-            GenSpawn.Spawn(this.atcDef,spIV,map,WipeMode.Vanish);
-            //Log.Message("Spawned ancient terminal");
+            if (spawnChance < 29f || !InternalDefOf.Fabrication.IsFinished) { Log.Message("[BTS] Skipped terminal generation. this is normal behaviour."); return; }
+            if ((checkForPassive && (map.IsPlayerHome || map.Parent is Camp)))
+            {
+                GenTerminal(spIV, map);
+                return;
+            }
+            else if (map.ParentFaction != null)
+            {
+                //Redundant but garunteed to get us what we want
+                if (!map.ParentFaction.IsPlayerSafe() || !map.ParentFaction.IsPlayer) GenTerminal(spIV, map);
+                return;
+            }
+            else
+            {
+                //Camps have a null parentfaction. makes sense.
+                if (checkForPassive) GenTerminal(spIV, map);
+                //else Log.Message("[BTS] Avoided generating terminal after all options attempted");
+                return;
+            }
+
+            //Log.Error("[BTS] Something went wrong trying to generate a landmark. We've encountered a map where this logic should not be run");
+            //return;
         }
 
         protected bool TryFindCell(Map map, out IntVec3 result)
